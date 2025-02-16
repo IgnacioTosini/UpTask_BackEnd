957c7bb2cb830036adbc6bea1bb2eae1be43471a
ignacio tosini <ignaciotosini2002@gmail.com>
2025-02-16 00:01:34 -0300

    upgrade

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -1,4 +1,8 @@
-import { transporter } from '../config/nodemailer'
+import { Resend } from 'resend'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const resend = new Resend(process.env.RESEND_API_KEY || '')
 
 interface IEmail {
     email: string
@@ -8,34 +12,30 @@ interface IEmail {
 
 export class AuthEmail {
     static sendConfirmationEmail = async (user: IEmail) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <admin@uptask.com>',
+        const info = await resend.emails.send({
+            from: 'UpTask <no-reply@resend.dev>',
             to: user.email,
             subject: 'UpTask - Confirma tu cuenta',
-            text: 'UpTask - Confirma tu cuenta',
-            html: `<p>Hola: ${user.name} has creado tu cuenta en UpTask, ya casi esta todo listo, solo debes confirmar tu cuenta</p>
+            html: `<p>Hola: ${user.name} has creado tu cuenta en UpTask, ya casi está todo listo, solo debes confirmar tu cuenta.</p>
                 <p>Visita el siguiente enlace:</p>
                 <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
                 <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
+                <p>Este token expira en 10 minutos</p>`
         })
-        console.log('Mensaje enviado', info.messageId)
+        console.log('Mensaje enviado', info.data.id)
     }
 
     static sendPasswordResetToken = async (user: IEmail) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <admin@uptask.com>',
+        const info = await resend.emails.send({
+            from: 'UpTask <no-reply@resend.dev>',
             to: user.email,
             subject: 'UpTask - Reestablece tu password',
-            text: 'UpTask - Reestablece tu password',
-            html: `<p>Hola: ${user.name} has solicitado reestablecer tu password</p>
+            html: `<p>Hola: ${user.name} has solicitado reestablecer tu password.</p>
                 <p>Visita el siguiente enlace:</p>
                 <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
                 <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
+                <p>Este token expira en 10 minutos</p>`
         })
-        console.log('Mensaje enviado', info.messageId)
+        console.log('Mensaje enviado', info.data.id)
     }
 }
